d585942cc3711412e02077bbd4715b534004b2dd
gabrielmodog <gabrielmodog@gmail.com>
2020-03-09 14:32:41 -0300

    fixes prices header

--- src/components/prices.js
+++ src/components/prices.js
@@ -1,41 +1,41 @@
-import React from 'react';
+import React from "react";
 
-import { calculatePerMinutes, calculateWithPlan } from '../functions';
+import { calculatePerMinutes, calculateWithPlan } from "../functions";
 
-const Prices = ({
-  origin,
-  receiver,
-  minutes,
-  calls,
-  plan,
-}) => (
+const Prices = ({ origin, receiver, minutes, calls, plan }) => (
   <div className="app__split">
     <div className="app__header">
-      <svg width="686" height="233" xmlns="http://www.w3.org/2000/svg">
+      <svg
+        style={{ width: "100%", fill: "#3C62C1" }}
+        height="233"
+        xmlns="http://www.w3.org/2000/svg"
+      >
         <g>
-          <path id="svg_2" d="m-1.373173,233.363747l706.88754,-234.743047l-707.583358,0l0.695818,234.743047z" strokeWidth="1.5" fill="#3c62c1" />
+          <polygon points="0,158.7 0,0 1280,0 " />
         </g>
       </svg>
-
     </div>
 
     <div className="app__prices">
       <div className="app__prices__box">
         <div className="app__prices__plan">
           <span>Plano Selecionado:</span>
-          {
-            plan ? <h3 className="bold-weight">{`FaleMais${plan}`}</h3> : <h3 className="bold-weight">Selecione um plano</h3>
-          }
+          {plan ? (
+            <h3 className="bold-weight">{`FaleMais${plan}`}</h3>
+          ) : (
+            <h3 className="bold-weight">Selecione um plano</h3>
+          )}
         </div>
       </div>
 
       <div className="app__prices__box">
-
         <div className="app__prices__between">
           <h3>sem FaleMais:</h3>
           <h3 className="app__prices__value">
             $
-            {origin && receiver ? calculatePerMinutes(calls[origin][receiver], minutes) : 0}
+            {origin && receiver
+              ? calculatePerMinutes(calls[origin][receiver], minutes)
+              : 0}
           </h3>
         </div>
 
@@ -43,10 +43,9 @@ const Prices = ({
           <h3>com FaleMais: </h3>
           <h3 className="app__prices__value">
             $
-            {
-              origin && receiver
-                ? calculateWithPlan(calls[origin][receiver], minutes, plan) : 0
-            }
+            {origin && receiver
+              ? calculateWithPlan(calls[origin][receiver], minutes, plan)
+              : 0}
           </h3>
         </div>
       </div>
@@ -57,6 +56,5 @@ const Prices = ({
     </div>
   </div>
 );
-
 
 export default Prices;
